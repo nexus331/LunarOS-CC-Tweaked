d6443c2c6971621d25181971ab0f1ec27f374fd5
nexus331 <110035736+nexus331@users.noreply.github.com>
2024-07-07 19:30:52 +0800

    Update lunarresmag.lua

--- lunarresmag.lua
+++ lunarresmag.lua
@@ -1,2 +1,18 @@
+-- Lunar Resource Manager Program
 print("Lunar Resource Manager 1.0")
 print("Please enter pin to start operation...")
+-- User Authentication
+
+local correctPassword = "0850"
+local passwordAsked = false
+local function askForPassword()
+    while true do
+        io.write("[LunarResMgr] Enter pin for user: ")
+        local password = read("*")
+        if password == correctPassword then
+            return true
+        else
+            print("[LunarResMgr] Incorrect pin.")
+        end
+    end
+end
